dfb29442df0637b1a7ddac468869f6a4a79cd5da
Raveen K Sharma <raveen08@gmail.com>
2021-02-06 21:00:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+let divide = function () {return 2000 / 100}
+
+//written this way, needs return, will not do auto like arrow
+
+let square = x => x ** 2;
+
+//left of the arrow is variable set, to the right is function body
+
+let add = (a, b) => a + b; 
+
+//when you have two variables, need parenthesis
